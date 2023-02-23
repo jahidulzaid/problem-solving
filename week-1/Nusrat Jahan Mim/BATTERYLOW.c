5c14728cfe2e566cdd08f2c475d2401dec03d360
Nusrat Jahan Mim <114955198+nusratmim43@users.noreply.github.com>
2023-02-23 22:59:22 +0600

    Update BATTERYLOW.c

--- week-1/Nusrat Jahan Mim/BATTERYLOW.c
+++ week-1/Nusrat Jahan Mim/BATTERYLOW.c
@@ -2,11 +2,13 @@
 
 int main(void) {
 	// your code goes here
-	int T,i;
+	int T;
 	scanf("%d",&T);
-	for(i=1;i<=T;i++)
+	for(int i=1;i<=T;i++)
 	{
-	if(T<=15){
+	    int x;
+	    scanf("%d",&x);
+	if(x<=15){
 	printf("YES\n");}
 	else 
 	printf("NO\n");
